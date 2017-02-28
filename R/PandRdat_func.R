1df4b222742ca63e115ab17d858ad9fd3a0d0061
EAWAG\schollje <jennifer.schollee@eawag.ch>
2017-02-28 17:05:42 +0100

    Made PandRdat function more flexible. Now able to deal with a data frame as input

--- R/PandRdat_func.R
+++ R/PandRdat_func.R
@@ -17,22 +17,22 @@ PandRdat <- function(realdata, randdata, metricList = metricList, steps = 0.001,
   
   PandRList <- list()
   
-  for(k in 1:length(metricList)){
+  if(is.null(metricList)){
     
     pandr <- data.frame(matrix(data = NA, nrow = 1/steps, ncol = 3))
     colnames(pandr) <- c("Threshold", "False pos", "True pos")
     
-    metric <- metricList[k]
-    real.npair <- length(realdata[,paste(metric)])
-    rand.npair <- length(randdata[,paste(metric)])
+  #  metric <- metricList[k]
+    real.npair <- length(realdata)
+    rand.npair <- length(randdata)
     
     
     for(i in 1:(1/steps+1)){
       
       thres <- (i-1)*steps
       pandr[i,1] <- thres
-      pandr[i,2] <- length(which(randdata[,paste(metric)] > thres)) / rand.npair
-      pandr[i,3] <- length(which(realdata[,paste(metric)] > thres)) / real.npair
+      pandr[i,2] <- length(which(randdata > thres)) / rand.npair
+      pandr[i,3] <- length(which(realdata > thres)) / real.npair
       
     }
     
@@ -40,14 +40,45 @@ PandRdat <- function(realdata, randdata, metricList = metricList, steps = 0.001,
     pandr[,5] <- 1 - pandr[,"False pos"]
     colnames(pandr) <- c("Threshold", "False pos", "True pos", "False neg", "True neg")
     
-   
-    #if(save.data == TRUE){
-    pandr$metric <- c(rep(metric, 1/steps+1))
-    PandRList[[k]] <- pandr
-    #   name(PandRList[k]) <- metric
-    #   return(PandRList)
-    #  }
     
+    #if(save.data == TRUE){
+   # pandr$metric <- c(rep(metric, 1/steps+1))
+    PandRList[[1]] <- pandr
+    
+  }else{
+    
+    for(k in 1:length(metricList)){
+      
+      pandr <- data.frame(matrix(data = NA, nrow = 1/steps, ncol = 3))
+      colnames(pandr) <- c("Threshold", "False pos", "True pos")
+      
+      metric <- metricList[k]
+      real.npair <- length(realdata[,paste(metric)])
+      rand.npair <- length(randdata[,paste(metric)])
+      
+      
+      for(i in 1:(1/steps+1)){
+        
+        thres <- (i-1)*steps
+        pandr[i,1] <- thres
+        pandr[i,2] <- length(which(randdata[,paste(metric)] > thres)) / rand.npair
+        pandr[i,3] <- length(which(realdata[,paste(metric)] > thres)) / real.npair
+        
+      }
+      
+      pandr[,4] <- 1 - pandr[,"True pos"]
+      pandr[,5] <- 1 - pandr[,"False pos"]
+      colnames(pandr) <- c("Threshold", "False pos", "True pos", "False neg", "True neg")
+      
+      
+      #if(save.data == TRUE){
+      pandr$metric <- c(rep(metric, 1/steps+1))
+      PandRList[[k]] <- pandr
+      #   name(PandRList[k]) <- metric
+      #   return(PandRList)
+      #  }
+      
+    }
   }
   
   return(PandRList)
