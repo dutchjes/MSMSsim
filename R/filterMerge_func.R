89ced2837f6f597c79198d8fe152fa5d23a6e2ad
jeschollee <jennifer.schollee@eawag.ch>
2019-06-26 15:43:59 +0200

    W10

--- R/filterMerge_func.R
+++ R/filterMerge_func.R
@@ -1,18 +1,19 @@
 #' Merging of spectra
 #'
-#' @param peaks Data frame with the peaks which are to be merged. Column 1 should be mz, column 2 should be intensity
+#' @param peaks Data frame with the peaks which are to be merged. Column 1 should be mz, column 2 should be absolute intensity
 #' @param dmz Delta m/z requested for merging, in Da. Default is 0.001
-#' @param int Intensity cutoff for merging
+#' @param int Intensity cutoff for merging. Currently not applied
 #'
-#' @return
+#' @return data frame with merged peaks. Column 1 is mz, column 2 is absolute intensity, and a new column 3 is the frequency of the fragment. Data frame is ordered by frequency
 #' @export
 #'
 #' @examples
 #' 
-filterMerge <- function (peaks, dmz = 0.001, int = 1e20) 
+filterMerge <- function (peaks, dmz = 0.001, int = 1e1) 
 {
   cutoff_int_limit <- int
   cutoff_mz_limit <- dmz
+  peaks <- data.frame(mz = peaks[,1], int = peaks[,2])
   peaks_o <- peaks[order(peaks$int, decreasing = TRUE), ]
   n <- 1
   peaks_o$count <- 0
